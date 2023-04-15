f3397ea503d6422c207b61abc472e266394ae128
Николай <gulyaev132415@yandex.ru>
2023-04-15 16:00:27 +0500

    asd

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,10 +6,16 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film addFilm(Film film);
+
     Film getFilm(int id);
+
     Collection<Film> getFilms();
+
     Film update(Film film);
+
     Film addLike(int filmId, int userId);
+
     Film deleteLike(int filmId, int userId);
+
     Collection<Film> getPopularityFilms(Integer count);
 }
