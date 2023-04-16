9aa181f3cd984ac56b62856d218f5ab0de4db953
Николай <gulyaev132415@yandex.ru>
2023-04-16 14:15:54 +0500

    asd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,13 +18,7 @@ CREATE TABLE IF NOT EXISTS genre
     id   INTEGER PRIMARY KEY,
     name VARCHAR(50)
 );
-INSERT INTO  genre (id, name) VALUES
-                (1, 'Комедия'),
-                (2, 'Драма'),
-                (3, 'Мультфильм'),
-              (4, 'Триллер'),
-                (5, 'Документальный'),
-                (6, 'Боевик');
+
     CREATE TABLE IF NOT EXISTS film
     (
         id          INTEGER PRIMARY KEY,
@@ -44,12 +38,7 @@ CREATE TABLE  IF NOT EXISTS mpa_rating
     mpa VARCHAR(6)
 
 );
-INSERT INTO  mpa_rating (id, mpa) VALUES
-             (1, 'G'),
-             (2, 'PG'),
-         (3, 'PG-13'),
-          (4, 'R'),
-          (5, 'NC-17');
+
 
 CREATE TABLE IF NOT EXISTS filmLikes
 (
