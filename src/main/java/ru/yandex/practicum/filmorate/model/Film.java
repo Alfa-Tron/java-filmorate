186d4178325379bbe13447d07d86ea1ebaf79930
Николай <gulyaev132415@yandex.ru>
2023-04-16 14:28:59 +0500

    asd

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -24,7 +24,7 @@ public class Film {
     @Min(1)
     private Long duration;
     private Set<Integer> likes = new HashSet<>();
-    private Integer rate = 0;
+    private Integer rate;
     private List<Genre> genres;
     private Mpa mpa;
 
