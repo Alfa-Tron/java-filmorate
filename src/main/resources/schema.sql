0822f6ec8ef6ac16718550346472279e1d0cb0fa
Николай <gulyaev132415@yandex.ru>
2023-04-16 17:57:11 +0500

    asd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,20 +18,30 @@ CREATE TABLE IF NOT EXISTS genre
     id   INTEGER PRIMARY KEY,
     name VARCHAR(50)
 );
-MERGE INTO genre g
-    USING (
-        SELECT 1 as id, 'Комедия' as name FROM DUAL
-        UNION ALL SELECT 2, 'Драма' FROM DUAL
-        UNION ALL SELECT 3, 'Мультфильм' FROM DUAL
-        UNION ALL SELECT 4, 'Триллер' FROM DUAL
-        UNION ALL SELECT 5, 'Документальный' FROM DUAL
-        UNION ALL SELECT 6, 'Боевик' FROM DUAL
-    ) new_data
-ON g.id = new_data.id
-WHEN NOT MATCHED THEN
-    INSERT (id, name) VALUES (new_data.id, new_data.name);
+MERGE INTO genre
+    KEY (id)
+    VALUES (1, 'Комедия'),
+           (2, 'Драма'),
+           (3, 'Мультфильм'),
+           (4, 'Триллер'),
+           (5, 'Документальный'),
+           (6, 'Боевик');
 
-    CREATE TABLE IF NOT EXISTS film
+CREATE TABLE IF NOT EXISTS mpa(
+                                         id INT PRIMARY KEY,
+                                         name VARCHAR(6)
+);
+
+
+MERGE INTO mpa
+    KEY (id)
+    VALUES (1, 'G'),
+           (2, 'PG'),
+           (3, 'PG-13'),
+           (4, 'R'),
+           (5, 'NC-17');
+
+CREATE TABLE IF NOT EXISTS film
     (
         id          INT AUTO_INCREMENT PRIMARY KEY,
         film_name   VARCHAR(50),
@@ -40,25 +50,8 @@ WHEN NOT MATCHED THEN
         duration    INTEGER,
         rate      INT,
         mpa INT,
-            FOREIGN KEY (mpa) REFERENCES mpa_rating(id)
+            FOREIGN KEY (mpa) REFERENCES mpa(ID)
     );
-CREATE TABLE IF NOT EXISTS mpa_rating(
-    id INT PRIMARY KEY,
-    name VARCHAR(6)
-);
-MERGE INTO mpa_rating g
-    USING (
-        SELECT 1 as id, 'G' as name FROM DUAL
-        UNION ALL SELECT 2, 'PG' FROM DUAL
-        UNION ALL SELECT 3, 'PG-13' FROM DUAL
-        UNION ALL SELECT 4, 'R' FROM DUAL
-        UNION ALL SELECT 5, 'NC-17' FROM DUAL
-
-    ) new_data
-ON g.id = new_data.id
-WHEN NOT MATCHED THEN
-    INSERT (id, name) VALUES (new_data.id, new_data.name);
-
 
 
 CREATE TABLE IF NOT EXISTS filmLikes
