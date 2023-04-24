c0400398f809dc2d1b733677e1c9c7b5a94b63a2
Николай <gulyaev132415@yandex.ru>
2023-04-24 16:51:56 +0500

    2

--- src/main/java/ru/yandex/practicum/filmorate/controllers/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/GenreController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Collection;
 
 @RestController
 public class GenreController {
+    @Autowired
     private GenreService genreService;
 
     @GetMapping("/genres")
