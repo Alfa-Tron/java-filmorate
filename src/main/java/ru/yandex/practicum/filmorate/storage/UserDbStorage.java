c92247d0ab42b6979f94b835243c91c9f469cae8
Николай <gulyaev132415@yandex.ru>
2023-04-25 23:21:32 +0500

    3

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.LocalDate;
 import java.util.*;
 
 @Component
